49ed81f9d1e49a9ab8d705bbd77840cb5b2e5239
noka <naoto.shevo.oka@gmail.com>
2026-02-05 19:38:44 -0500

    debug gitignore

---
+++ src/lib/userIds.ts
@@ -0,0 +1,6 @@
+// User IDs for development/testing - DO NOT COMMIT
+export const USER_IDS = {
+  ROGER_1: "80fb16d2-6df8-4500-bdca-524645a544ca",
+  ROGER_2: "3d120d6e-c9dd-4284-afdb-88693d32811f",
+  ROGER_3: "3caa0904-5aa6-4e7a-ad80-6d9bc0a6be0b",
+} as const;
